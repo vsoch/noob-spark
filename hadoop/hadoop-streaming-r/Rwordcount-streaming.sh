5ce9f5aa32253975f7c8b9e107280fb9ad205b9c
vsoch <vsochat@stanford.edu>
2016-05-01 15:29:45 -0700

    preparing to test python streaming

--- hadoop/hadoop-streaming-r/Rwordcount-streaming.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-hadoop jar $HADOOP_STREAMING \
--D mapred.map.tasks=4 -D mapred.reduce.tasks=2 \
--file mapper-2.R -file runRMapper.sh -mapper runRMapper.sh \
--file reducer-4.R -file runRReducer.sh -reducer runRReducer.sh \
--input /user/$USER/data/book.txt  -output /user/$USER/output-streaming
--- hadoop/hadoop-streaming-r/runRReducer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export LIBRARY_PATH=/opt/apps/intel/15/composer_xe_2015.3.187/ipp/lib/intel64:/opt/apps/intel/15/composer_xe_2015.3.187/mkl/lib/intel64:/opt/apps/intel/15/composer_xe_2015.3.187/tbb/lib/intel64:/opt/apps/intel/15/composer_xe_2015.3.187/compiler/lib/intel64
-export LD_LIBRARY_PATH=/opt/apps/gcc/4.9.1/lib64:/opt/apps/gcc/4.9.1/lib:/opt/apps/intel15/mvapich2_2_1/Rstats/3.2.1/jags-3.4.0/lib64/JAGS/modules-3:/opt/apps/intel15/mvapich2_2_1/Rstats/3.2.1/jags-3.4.0/lib64:/opt/apps/intel15/mvapich2_2_1/Rstats/3.2.1/proj-4.7.0/lib:/opt/apps/intel15/mvapich2_2_1/Rstats/3.2.1/protobuf-4.7.1/lib:/opt/apps/intel15/mvapich2_2_1/Rstats/3.2.1/gdal-1.9.2/lib:/opt/apps/intel15/mvapich2_2_1/Rstats/3.2.1/lib64/R/lib:/opt/apps/intel15/mvapich2/2.1/lib:/opt/apps/intel15/mvapich2/2.1/lib/shared:/opt/apps/intel/15/composer_xe_2015.3.187/mpirt/lib/intel64:/opt/apps/intel/15/composer_xe_2015.3.187/ipp/lib/intel64:/opt/apps/intel/15/composer_xe_2015.3.187/mkl/lib/intel64:/opt/apps/intel/15/composer_xe_2015.3.187/tbb/lib/intel64:/opt/apps/intel/15/composer_xe_2015.3.187/tbb/lib/intel64/gcc4.4:/opt/apps/intel/15/composer_xe_2015.3.187/compiler/lib/intel64
-
-./reducer-4.R
